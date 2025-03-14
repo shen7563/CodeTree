5a3802b54bb3c167ee320ed4a4c6fd4090ae23e1
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-15 08:27:06 +0900

    Title: 문자 변경하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/문자 변경하기/change-charater.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    char a = 'C';
+    a = 'T';
+    cout << a;
+}
