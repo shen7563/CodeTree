e1cf96643ea0036671c7e8087f8bba09b9b7b74d
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-15 08:09:56 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a = 7;
+    int b = 23;
+    int c = 30;
+    cout << a << " + " << b << " = " << c;
+}
