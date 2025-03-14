9219e7ce4b499b49c070b28b91ff27d5a2218a7f
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-15 08:21:29 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout << fixed;
+    cout.precision(1);
+    
+    double a = 25.352;
+    
+    cout << a;
+}
