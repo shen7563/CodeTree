bdf8522a3f30dfd850ba9a3f35f45ad1dba97d60
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-15 08:47:18 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    a = b = c;
+
+    cout << a << " " << b << " " << c;
+}
