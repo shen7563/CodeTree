0ea1bc42d85855d0706412f1d4b24d6f7f07b7f1
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-15 08:11:14 +0900

    Title: 변수 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a = 3;
+    char b = 'C';
+    cout << b << "!.....!" << a;
+}
