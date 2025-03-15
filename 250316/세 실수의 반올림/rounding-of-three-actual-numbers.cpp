9276438d397ec245ce6159bb28da738699ec9153
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 01:01:48 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 0;
+    double b = 0;
+    double c = 0;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << a << "\n" << b << "\n" << c;
+}
