894287dade4fb3a3728c164d16ee8ddfa7366ea8
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-15 23:31:45 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 0;
+
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a + 1.5;
+    return 0;
+}
