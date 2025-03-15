17b6c5f427e345dc16729b8fa22eb5561cc364a1
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 03:05:18 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int a = 0;
+    int b=0;
+    cin >> a >> b;
+
+    cout << a + b << "\n" << a - b << "\n"
+    << a / b << "\n" << a % b;
+
+}
