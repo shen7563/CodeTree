552bb5766b9eb95473c7a0c3a8fe83182cee9f8a
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 03:47:17 +0900

    Title: 큰 수에서 빼기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    cin >> a >> b;
+
+    if(a >= b) {
+        cout << a - b;
+    }
+    if(a < b) {
+        cout << b - a;
+    }
+}
