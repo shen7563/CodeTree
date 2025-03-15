9f81c7fccc2d7c02bee164006d83f9099b1780d0
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 02:27:11 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    int front = 0;
+    int back = 0;
+
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << front << back;
+}
