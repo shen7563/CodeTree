33d4b56b174cc44360b4572f94d74e05964e4e1d
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 03:46:19 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 0;
+
+    cin >> a;
+    cout << a << "\n";
+    
+    if(a < 0) {
+        cout << "minus";
+    }
+    return 0;
+}
