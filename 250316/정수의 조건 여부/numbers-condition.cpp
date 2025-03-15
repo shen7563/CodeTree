a7360ee547b9f36821a10a0a677d47b714f052c1
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 04:09:01 +0900

    Title: 정수의 조건 여부, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    cin >> a;
+
+    if(a >= 113) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}
