80a07121a58d25c597cc9a5478f1d76d2f2c24a1
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 02:24:56 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cout << a + 1 << ":" << b;
+}
