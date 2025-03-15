e2d762990fb58b1f0a83e43121f341a3eba32dd4
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-16 04:32:34 +0900

    Title: 살 수 있는 물건, Time: 110ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250316/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int money = sc.nextInt();
+        
+        if(money >= 3000) {
+            System.out.println("book");
+        }
+        else if(money >= 1000) {
+            System.out.println("mask");
+        }
+        else {
+            System.out.println("no");
+        }
+    }
+}
