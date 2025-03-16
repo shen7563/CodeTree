fdaa624d8e69f94f4523ec7451a05bb2fa0d5172
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-17 05:11:40 +0900

    Title: 수의 계절은, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250317/수의 계절은/season-of-num.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int month = sc.nextInt();
+
+        if(month >= 3 && month <= 5) {
+            System.out.println("Spring");
+        } else if(month >= 6 && month <= 8) {
+            System.out.println("Summer");
+        } else if(month >= 9 && month <= 11) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("Winter");
+        }
+    }
+}
