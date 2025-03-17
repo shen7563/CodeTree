f58f03319221eadcc4b36c4d7cc9a1f3561e857e
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-17 23:53:20 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250317/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 26;
+    int i = n;
+
+    while(i >= 10) {
+        cout << i << " ";
+        i--;
+    }
+    return 0;
+}
