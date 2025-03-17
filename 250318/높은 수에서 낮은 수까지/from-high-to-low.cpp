9652583e77857eb8b4455ea7d4fa879409e8278e
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-18 00:58:53 +0900

    Title: 높은 수에서 낮은 수까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250318/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b = 0;
+    cin >> a >> b;
+
+    if(a >= b) {
+        for(int i = a; i >= b; i--) {
+            cout << i << " ";
+        }
+    }
+    else {
+        for(int i = b; i >= a; i--) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
