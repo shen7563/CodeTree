197eed44bb42bbb1d15bca78e1090d5bd6df2bab
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-17 20:59:22 +0900

    Title: 윤년인가, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250317/윤년인가/is-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0 && year % 400 != 0) {
+                System.out.println("false");
+            } else {
+                System.out.println("true");
+            }
+        } else {
+            System.out.println("false");
+        }
+    }
+}
