49e9ad407c5b00550f0ee039d76f6a8e3db79896
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-20 05:07:42 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250320/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
