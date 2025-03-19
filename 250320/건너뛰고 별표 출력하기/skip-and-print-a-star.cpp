fde01c5936fc4faddd4b3e7e28037d350522465f
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-20 05:55:39 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250320/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n\n";
+    }
+    for(int i = 1; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            cout << "*";
+        }
+        cout << "\n\n";
+    }
+    return 0;
+}
