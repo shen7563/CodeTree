b54517b1e0a62b6dd27136d601e805258c6e283f
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-20 13:31:51 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250320/별표 출력하기 9/print-star-9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n - 1; j++) {
+            cout << "  ";
+        }
+        for(int j = 0; j < 2 * i + 1; j++) {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
