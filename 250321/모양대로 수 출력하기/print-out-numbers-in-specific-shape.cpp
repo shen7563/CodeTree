3d408298761d6652b6d4dfe4b05b94003c8d0ff6
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-21 20:17:08 +0900

    Title: 모양대로 수 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250321/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            cout << "  ";
+        }
+        for(int j = n - i; j > 0; j--) {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
