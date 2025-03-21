b5942560a3e47b54c8da5579a33c8cb533f347ce
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-21 19:12:40 +0900

    Title: 구구단 만들기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250321/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = 1; i <= 9; i++) {
+            for(int j = b; j >= a; j -= 2) {
+                System.out.printf("%d * %d = %d", j, i, j * i);
+                if (j != a) {
+                    System.out.print(" / ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
