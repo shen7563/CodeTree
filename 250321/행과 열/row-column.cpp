f165d104e6d251dc7c53500fa0775da99502d05a
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-21 12:09:51 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250321/행과 열/row-column.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n = 0, m = 0;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout << i * j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
