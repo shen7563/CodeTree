91d1b2c549720124858fea6187af499b471080ab
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-21 19:16:01 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250321/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int n = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << i << " * " << j << " = " << i * j;
+            if(j != n) {
+                cout << ", ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
