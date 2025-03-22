d24678ae68b4449ef51056264e4f3d314ad26403
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-23 00:59:08 +0900

    Title: 알파벳 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250323/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0, alpha = 65;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(alpha > 90) {
+                alpha = 65;
+            }
+            if(i > j) {
+                cout << "  ";
+            } else {
+                cout << (char)alpha << " ";
+                alpha++;
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
