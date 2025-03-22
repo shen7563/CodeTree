26b99727a41dbcc91a82abe651ed468dca5d5891
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-23 04:28:59 +0900

    Title: 특정 수 도달하기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250323/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int sum = 0;
+        double avg = 0;
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < 10; i++) {
+            if(arr[i] >= 250) {
+                avg = (double)sum / i;
+                break;
+            }
+            else {
+                sum += arr[i];
+            }
+        }
+        System.out.printf("%d %.1f", sum, avg);
+        sc.close();
+    }
+}
