06054fee21e4821a989242eae592f30b7d4b822a
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-23 00:49:47 +0900

    Title: 알파벳 출력하기 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250323/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int alpha = 65;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(alpha > 90) {
+                    alpha = 65;
+                }
+                if(i > j) {
+                    System.out.print("  ");
+                }
+                else {
+                    System.out.print((char)alpha + " ");
+                    alpha++;
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
