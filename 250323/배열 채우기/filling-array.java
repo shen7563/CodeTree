9d0ab753d8bbe4fb953dc17cbe117ff11260dee3
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-23 04:56:51 +0900

    Title: 배열 채우기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250323/배열 채우기/filling-array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int count = 0;
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0) {
+                break;
+            }
+            count++;
+        }
+        for(int i = count - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " "); 
+        }
+        sc.close();
+    }
+}
