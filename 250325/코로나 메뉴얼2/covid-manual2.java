34dfba46339fcc8dae3ea4109bb5d3315b68eb3f
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-25 01:01:58 +0900

    Title: 코로나 메뉴얼2, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250325/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sym[] = new String[3];
+        int temp[] = new int[3];
+        int count = 0;
+        int emergency[] = new int[4];
+        for(int i = 0; i < 3; i++) {
+            sym[i] = sc.next();
+            temp[i] = sc.nextInt();
+            if(sym[i].charAt(0)== 'Y') {
+                if(temp[i] >= 37) {
+                    emergency[0]++;
+                    count++;
+                }
+                else {
+                    emergency[1]++;
+                }
+            }
+            else {
+                if(temp[i] >= 37) {
+                    emergency[2]++;
+                }
+                else {
+                    emergency[3]++;
+                }
+            }
+        }
+        for(int i = 0; i < 4; i++) {
+            System.out.print(emergency[i] + " ");
+        }
+        if(count >= 2) {
+            System.out.print("E");
+        }
+        sc.close();
+    }
+}
