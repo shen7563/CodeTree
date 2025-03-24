38e97c696a132a38acc2ca631eda12e2b1b64d4b
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-03-25 01:47:42 +0900

    Title: 점수대 파악하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250325/점수대 파악하기/find-out-the-score-range.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int score[100] = {0, };
+    int scoreCount[11] = {0, };
+    for(int i = 0; i < 100; i++)
+    {
+        cin >> score[i];
+        scoreCount[score[i] / 10]++;
+    }
+    for(int i = 10; i >= 1; i--) {
+        cout << i * 10 << " - " << scoreCount[i] << "\n";
+    }
+    return 0;
+}
