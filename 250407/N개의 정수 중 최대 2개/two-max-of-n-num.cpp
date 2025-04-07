c1df2bef7e31255465c935e17dbc03a989014f4f
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-07 22:19:39 +0900

    Title: N개의 정수 중 최대 2개, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250407/N개의 정수 중 최대 2개/two-max-of-n-num.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int arr[100] = {0};
+    int max_first = INT_MIN;
+    int max_second = INT_MIN;
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(arr[i] > max_first) {
+            max_second = max_first;
+            max_first = arr[i];
+        } else if(arr[i] > max_second && arr[i] != max_first) {
+            max_second = arr[i];
+        }
+    }
+    cout << max_first << " " << max_second;
+    return 0;
+}
