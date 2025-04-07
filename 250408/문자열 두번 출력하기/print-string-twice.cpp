3389d4403721377818e7442ee415d68b28401a4f
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-08 02:30:51 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string str;
+    cin >> str;
+    cout << str << "\n" << str;
+    return 0;
+}
