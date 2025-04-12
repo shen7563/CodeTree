a0a83e7aa3f6c92d8fa5727ce5ff3f38936a1cf2
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-13 03:09:07 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250413/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    string str[10];
+    for(int i = 0; i < 10; i++) {
+        cin >> str[i];
+        cout << str[i] << "\n";
+    }
+    return 0;
+}
