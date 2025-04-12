df3d1a281e1844f6716aea2c20abf8533b40fd2b
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-12 23:24:42 +0900

    Title: 공백을 기준으로 출력, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250412/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        
+        for(int i = 0; i < str1.length(); i++) {
+            if(str1.charAt(i) == ' ') {
+                continue;
+            }
+            System.out.print(str1.charAt(i));
+        }
+        for(int i = 0; i < str2.length(); i++) {
+            if(str2.charAt(i) == ' ') {
+                continue;
+            }
+            System.out.print(str2.charAt(i));
+        }
+        sc.close();
+    }
+}
