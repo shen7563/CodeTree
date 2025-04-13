4a66289da2c831e8c695f2a52b0b7c5304f56b58
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-14 07:20:44 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;
+    cout << s + "Hello";
+    return 0;
+}
