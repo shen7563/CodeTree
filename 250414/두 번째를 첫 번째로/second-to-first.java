5e8f91ea759752fb9026d8f02f7c130f2a757330
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-14 08:57:07 +0900

    Title: 두 번째를 첫 번째로, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250414/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+        char x = arr[1];
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                arr[i] = arr[0];
+            }
+        }
+        s = String.valueOf(arr);
+        System.out.println(s);
+        sc.close();
+    }
+}
