5421fa3ff117fccb78da226d79e8e5c6b28e98e4
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-14 07:31:45 +0900

    Title: 특정 문자의 유무, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250414/특정 문자의 유무/specific-character-presence.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        if(s.contains("ee")) {
+            System.out.print("Yes");
+        }
+        else {
+            System.out.print("No");
+        }
+
+        if(s.contains("ab")) {
+            System.out.print(" Yes");
+        }
+        else {
+            System.out.print(" No");
+        }
+        sc.close();
+    }
+}
