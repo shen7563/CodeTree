07705badd58d7bf816d9e1f469897bcf0985a247
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-15 00:24:34 +0900

    Title: 아스키코드 변환, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250415/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    char a;
+    int b;
+    cin >> a >> b;
+    cout << (int)a << " " << (char)b;
+    return 0;
+}
