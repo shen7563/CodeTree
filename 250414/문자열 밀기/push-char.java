f717743274400e78dcb9b6f2a519e3907a130002
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-14 23:24:56 +0900

    Title: 문자열 밀기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250414/문자열 밀기/push-char.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        s = s.substring(1, s.length()) + s.substring(0, 1);
+        System.out.println(s);
+        sc.close();
+    }
+}
