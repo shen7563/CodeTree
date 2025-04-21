86b512c8cd47482684d42514627cb657119312ae
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-04-22 02:05:00 +0900

    Title: 2개씩 그룹짓기, Time: 369ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250422/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[2 * n];
+        int[] arr1 = new int[2 * n];
+        int[] answer = new int[2 * n];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < 2 * n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i = 0; i < 2 * n; i++) {
+            arr1[i] = arr[i];
+        }
+        Integer[] arr2 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr2, Collections.reverseOrder());
+        for(int i = 0; i < n; i++) {
+            answer[i] = arr1[i] + arr2[i];
+            if(answer[i] > max) {
+                max = answer[i];
+            }
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
