ecc53dc82291ba1402c1d9c7fdaea20920737584
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-05-05 02:18:57 +0900

    Title: 연속되는 수 4, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250505/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n;
+int arr[MAX_N];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int count = 1;
+    int maxCount = INT_MIN;
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > arr[i - 1]) {
+            count++;
+        }
+        else {
+            maxCount = max(maxCount, count);
+            count = 1;
+        }
+    }
+    maxCount = max(maxCount, count);
+    cout << maxCount << "\n";
+    return 0;
+}
