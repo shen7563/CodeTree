e2b7268fe105d081ae57ba21487caa13cfab2687
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-05-06 00:25:04 +0900

    Title: 좌우로 움직이는 로봇, Time: 1240ms, Memory: 59MB, Status: Passed - Codetree

--- 250506/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250506/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -5,7 +5,7 @@ public class Main {
     public static final int MAX_N = 1000001;
     public static int[] aMove = new int[MAX_N];
     public static int[] bMove = new int[MAX_N];
-    public static boolean[] isVisited = new boolean[MAX_N];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -13,48 +13,52 @@ public class Main {
         int m = sc.nextInt();
         int aTime = 1;
         int bTime = 1;
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
             String a_move = sc.next();
-            if(a_move.equals("R")) {
-                for(int j = 0; j < a; j++) {
+            if (a_move.equals("R")) {
+                for (int j = 0; j < a; j++) {
                     aMove[aTime] = aMove[aTime - 1] + 1;
                     aTime++;
                 }
-            }
-            else {
-                for(int j = 0; j < a; j++) {
+            } else {
+                for (int j = 0; j < a; j++) {
                     aMove[aTime] = aMove[aTime - 1] - 1;
                     aTime++;
                 }
             }
         }
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             int b = sc.nextInt();
             String b_move = sc.next();
-            if(b_move.equals("R")) {
-                for(int j = 0; j < b; j++) {
+            if (b_move.equals("R")) {
+                for (int j = 0; j < b; j++) {
                     bMove[bTime] = bMove[bTime - 1] + 1;
                     bTime++;
                 }
-            }
-            else {
-                for(int j = 0; j < b; j++) {
+            } else {
+                for (int j = 0; j < b; j++) {
                     bMove[bTime] = bMove[bTime - 1] - 1;
                     bTime++;
                 }
             }
         }
+        if(aTime > bTime) {
+            for (int i = bTime; i < aTime; i++) {
+                bMove[i] = bMove[bTime - 1];
+            }
+        } else if(bTime > aTime) {
+            for (int i = aTime; i < bTime; i++) {
+                aMove[i] = aMove[aTime - 1];
+            }
+        }
         int meetCount = 0;
-        for(int i = 1; i < Math.max(aTime, bTime); i++) {
+        for (int i = 1; i < Math.max(aTime, bTime); i++) {
             if(aMove[i] == bMove[i]) {
-                if(isVisited[aMove[i]]) {
+                if(i == 1 || (aMove[i - 1] == bMove[i - 1])) {
                     continue;
                 }
-                else {
-                    meetCount++;
-                }
-                isVisited[aMove[i]] = true;
+                meetCount++;
             }
         }
         System.out.println(meetCount);
