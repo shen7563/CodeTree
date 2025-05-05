10e896cf343565f90d76fac5d1f0e4aca75da470
shen7563 <118192095+shen7563@users.noreply.github.com>
2025-05-05 22:38:56 +0900

    Title: 만나는 그 순간, Time: 218ms, Memory: 28MB, Status: Passed - Codetree

--- 250505/만나는 그 순간/the-moment-we-meet.java
+++ 250505/만나는 그 순간/the-moment-we-meet.java
@@ -2,53 +2,63 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final int MAX_N = 1000000;
+    public static final int MAX_N = 1000001;
     public static int[] aMoves = new int[MAX_N];
     public static int[] bMoves = new int[MAX_N];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int a_move = 0, b_move = 0;
-        int a_idx = 0, b_idx = 0;
-        for(int i = 0; i < n; i++) {
+        int a_idx = 1;
+        int b_idx = 1;
+        int a_moves = 0;
+        int b_moves = 0;
+        for (int i = 0; i < n; i++) {
             String s = sc.next();
-            int a_d = sc.nextInt();
-            if(s.equals("R")) {
-                for(int j = 0; j < a_d; j++) {
-                    aMoves[a_idx++] = a_move;
-                    a_move++; 
+            int a = sc.nextInt();
+            if (s.equals("R")) {
+                for (int j = 0; j < a; j++) {
+                    a_moves++;
+                    aMoves[a_idx++] = a_moves;
                 }
-            }
-            else {
-                for(int j = 0; j < a_d; j++) {
-                    aMoves[a_idx++] = a_move;
-                    a_move--;
+            } else {
+                for (int j = 0; j < a; j++) {
+                    a_moves--;
+                    aMoves[a_idx++] = a_moves;
                 }
             }
         }
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             String s = sc.next();
-            int b_d = sc.nextInt();
-            if(s.equals("R")) {
-                for(int j = 0; j < b_d; j++) {
-                    bMoves[b_idx++] = b_move;
-                    b_move++; 
+            int b = sc.nextInt();
+            if (s.equals("R")) {
+                for (int j = 0; j < b; j++) {
+                    b_moves++;
+                    bMoves[b_idx++] = b_moves;
                 }
-            }
-            else {
-                for(int j = 0; j < b_d; j++) {
-                    bMoves[b_idx++] = b_move;
-                    b_move--;
+            } else {
+                for (int j = 0; j < b; j++) {
+                    b_moves--;
+                    bMoves[b_idx++] = b_moves;
                 }
             }
         }
-        for(int i = 1; i < MAX_N; i++) {
-            if(aMoves[i] == bMoves[i]) {
-                System.out.println(i);
+        int max_idx = Math.max(a_idx, b_idx);
+        boolean isMeet = false;
+        int meets = 0;
+        for (int i = 1; i < max_idx; i++) {
+            if (aMoves[i] == bMoves[i]) {
+                isMeet = true;
+                meets = i;
                 break;
             }
+        }
+        if (isMeet) {
+            System.out.println(meets);
+        } else {
+            System.out.println(-1);
         }
         sc.close();
     }
